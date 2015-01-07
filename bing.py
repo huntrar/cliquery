29314198233a88e9cb81b3b0144d3548e8a19ca6
Hunter H <huntrar@gmail.com>
2015-01-07 15:23:23 -0800

    modularized code

--- bing.py
+++ bing.py
@@ -1,163 +1,201 @@
 ''' Python 2.7.3
 AUTHOR: Hunter Hammond
-VERSION: 1.8
+VERSION: 1.9
 DEPENDENCIES: lxml
 '''
 
+import argparse
 import sys
 import urllib2
 import re
 
 import lxml.html as lh
 
-# Only one flag may be found
-flags = { 'found' : False,
-          's' : False,
-          'f' : False,
-          'o' : False,
-          'w' : False,
-}
 
-# Clean arguments
-arg_list = list(sys.argv)
-arg_list.pop(0) # Pop script name from list
-clean_args = []
-if arg_list:
-    for arg in arg_list:
-        try:
-            if arg[0] == "-" and not flags['found']:
+class BingSearch:
+    def __init__(self, url_args, search_flag, f_lucky_flag, open_flag, wolfram_flag):
+       self.search_flag = search_flag
+       self.f_lucky_flag = f_lucky_flag
+       self.open_flag = open_flag
+       self.wolfram_flag = wolfram_flag
+       self.url_args = self.ProcessArgs(url_args)
+       self.html = self.GetBingHTML(self.url_args)
+
+    def ProcessArgs(self, url_args):
+        clean_args = []
+        if url_args:
+            for arg in url_args:
                 try:
-                    arg_flag = arg[1]
-                    flags[arg_flag] = True
-                    flags['found'] = True
+                    if " " in arg:
+                        clean_args = arg.split(" ")
+                    else:
+                        clean_args.append(arg)
                 except IndexError:
-                    pass
-            else:
-                if " " in arg:
-                    clean_args = arg.split(" ")
-                else:
-                    clean_args.append(arg)
-        except IndexError:
+                    print 'BingFail'
+                    sys.stderr.write('No search terms entered.\n')
+                    sys.exit()
+        else:
             print 'BingFail'
             sys.stderr.write('No search terms entered.\n')
-            sys.exit(1)
-    if flags['o']:
-        print ' '.join(clean_args) + 'BingOpen'
-        sys.exit(0)
-else:
-    print 'BingFail'
-    sys.stderr.write('No search terms entered.\n')
-    sys.exit(1)
+            sys.exit()
 
-if flags['w']:
-    print 'WolfFlag'
-    sys.exit(0)
+        # Further cleaning of args before they are added to base_url
+        new_url_args = []
+        if not self.open_flag:
+            try:
+                for url_arg in clean_args:
+                    if "+" in url_arg:
+                        url_arg = arg.replace('+', '%2B')
+                    new_url_args.append(url_arg)
+                if len(new_url_args) > 1:
+                    new_url_args = '+'.join(url_args)
+                else:
+                    new_url_args = url_args[0]
+            except IndexError:
+                print 'BingFail'
+                sys.stderr.write('No search terms entered.\n')
+                sys.exit()
+        else:
+            for url_arg in clean_args:
+                if ".com" not in url_arg:
+                    new_url_args.append(url_arg + ".com")
+                else:
+                    new_url_args.append(url_arg)
+        return new_url_args
+       
+    def GetBingHTML(self, url_args): 
+        if not self.open_flag:
+            base_url = 'http://www.bing.com/search?q='
+            url = base_url + url_args
+            try:
+                request = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+                return lh.parse(urllib2.urlopen(request))
+            except urllib2.URLError:
+                print 'BingFail'
+                sys.stderr.write('Failed to retrieve webpage.\n')
+                sys.exit()
 
-# Further cleaning of args before they are added to base_url
-url_args = []
-try:
-    for url_arg in clean_args:
-        if "+" in url_arg:
-            url_arg = arg.replace('+', '%2B')
-        url_args.append(url_arg)
-    if len(url_args) > 1:
-        url_args = '+'.join(url_args)
-    else:
-        url_args = url_args[0]
-except IndexError:
-    print 'BingFail'
-    sys.stderr.write('No search terms entered.\n')
-    sys.exit(1)
-    
-base_url = 'http://www.bing.com/search?q='
-url = base_url + url_args
-
-# Retrieve webpage response
-try:
-    request = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-    html = lh.parse(urllib2.urlopen(request))
-except urllib2.URLError:
-    print 'BingFail'
-    sys.stderr.write('Failed to retrieve webpage.\n')
-    sys.exit(0)
-
-# Parse webpage response
-if flags['f']:
-    try:
+    def BingSearchResults(self, html):
         unprocessed_links = html.xpath('//h2/a/@href')
+        if not unprocessed_links:
+            print 'BingFail'
+            sys.stderr.write('Failed to retrieve webpage.\n')
+            sys.exit()
+        links = []
+        link_descs = []
         for link in unprocessed_links:
-            if not re.search('(ad|Ad|AD)(?=\W)', link): # Ad check
-                if "http://" in link or "https://" in link:
-                    print link + 'BingFL'
-                    sys.exit(0) # Exit once first valid link printed
+            if not re.search('(ad|Ad|AD)(?=\W)', link): # Basic ad blocker
+                if "http://" in link or "https://" in link: 
+                    links.append(link)
+                    ld_xpath = "//h2/a[@href='" + str(link) + "']//text()"
+                    link_desc = html.xpath(ld_xpath)
+                    if type(link_desc) == list:
+                        link_desc = ''.join(link_desc)
+                    link_descs.append(link_desc)
                 elif '/images/' in link:
-                    link = 'http://www.bing.com' + link
-                    print link + 'BingFL'
-                    sys.exit(0) # Exit once first valid link printed
-    except IndexError:
-        print 'BingFail'
-        sys.stderr.write('Failed to retrieve webpage.\n')
-        sys.exit(1)
+                    links.append('http://www.bing.com' + link)
+                    ld_xpath = "//h2/a[@href='" + str(link) + "']//text()"
+                    link_desc = html.xpath(ld_xpath)
+                    if type(link_desc) == list:
+                        link_desc = ''.join(link_desc)
+                    link_descs.append(link_desc)
 
-elif flags['s']:
-    unprocessed_links = html.xpath('//h2/a/@href')
-    if not unprocessed_links:
-        print 'BingFail'
-        sys.stderr.write('Failed to retrieve webpage.\n')
-        sys.exit(1)
-    links = []
-    link_descs = []
-    for link in unprocessed_links:
-        if not re.search('(ad|Ad|AD)(?=\W)', link): # Check for advertisement
-            if "http://" in link or "https://" in link: 
-                links.append(link)
-                ld_xpath = "//h2/a[@href='" + str(link) + "']//text()"
-                link_desc = html.xpath(ld_xpath)
-                if type(link_desc) == list:
-                    link_desc = ''.join(link_desc)
-                link_descs.append(link_desc)
-            elif '/images/' in link:
-                links.append('http://www.bing.com' + link)
-                ld_xpath = "//h2/a[@href='" + str(link) + "']//text()"
-                link_desc = html.xpath(ld_xpath)
-                if type(link_desc) == list:
-                    link_desc = ''.join(link_desc)
-                link_descs.append(link_desc)
+        if links and link_descs:
+            for i in xrange(len(links)):
+                print_desc = (str(i) + ". " + link_descs[i] + "\n").encode('ascii', 'ignore')
+                sys.stderr.write(print_desc) # Prints link choices
 
-    if links and link_descs:
-        for i in xrange(len(links)):
-            print_desc = (str(i) + ". " + link_descs[i] + "\n").encode('ascii', 'ignore')
-            sys.stderr.write(print_desc) # Prints link choices
+            try:
+                sys.stderr.write(": ")
+                link_num = raw_input("")
+                if(link_num):
+                    print(links[int(link_num)]) + 'BingPage'
+                    sys.exit()
+            except (ValueError, IndexError):
+                print 'qBingPage'
+                sys.exit()
 
+    def FeelingLucky(self, html):
         try:
-            sys.stderr.write(": ")
-            link_num = raw_input("")
-            if(link_num):
-                print(links[int(link_num)]) + 'BingPage'
-                sys.exit(0)
-        except (ValueError, IndexError):
-            print 'qBingPage'
-            sys.exit(1)
+            unprocessed_links = html.xpath('//h2/a/@href')
+            for link in unprocessed_links:
+                if not re.search('(ad|Ad|AD)(?=\W)', link): # Basic ad block
+                    if "http://" in link or "https://" in link:
+                        print link + 'BingFL'
+                        sys.exit() # Exit once first valid link printed
+                    elif '/images/' in link:
+                        link = 'http://www.bing.com' + link
+                        print link + 'BingFL'
+                        sys.exit() # Exit once first valid link printed
+        except IndexError:
+            sys.stderr.write('Failed to retrieve webpage.\n')
+            sys.exit()
 
-# Check for Bing calculation/definition result
-calc_result = html.xpath('//span[@id="rcTB"]/text()|//div[@class="b_focusTextMedium"]/text()|//p[@class="b_secondaryFocus df_p"]/text()|//div[@class="b_xlText b_secondaryText"]/text()|//input[@id="uc_rv"]/@value') # Check if calculation result is present or age/date
-define_result = html.xpath('//ol[@class="b_dList b_indent"]/li/div/text()') # Check if calculation result is a definition
-try:
-    if calc_result:
-        if len(calc_result) == 1:
-            print (calc_result[0] + 'BingCalc').encode('ascii', 'ignore')
+    def Wolfram(self):
+        print 'WolfFlag'
+        sys.exit()
+
+    def BingCalculation(self, html):
+        calc_result = html.xpath('//span[@id="rcTB"]/text()|//div[@class="b_focusTextMedium"]/text()|//p[@class="b_secondaryFocus df_p"]/text()|//div[@class="b_xlText b_secondaryText"]/text()|//input[@id="uc_rv"]/@value') # Check if calculation result is present or age/date
+        define_result = html.xpath('//ol[@class="b_dList b_indent"]/li/div/text()') # Check if calculation result is a definition
+        try:
+            if calc_result:
+                if len(calc_result) == 1:
+                    print (calc_result[0] + 'BingCalc').encode('ascii', 'ignore')
+                else:
+                    print ('\n'.join(calc_result) + 'BingCalc').encode('ascii', 'ignore')
+                sys.exit()
+            elif define_result:
+                if len(define_result) == 1:
+                    print (define_result[0] + 'BingCalc').encode('ascii', 'ignore')
+                else:
+                    print ('\n'.join(define_result) + 'BingCalc').encode('ascii', 'ignore')
+                sys.exit()
+            #else:
+                #print 'BingNoMatch'
+                #sys.exit()
+        except AttributeError:
+            pass
+            #print 'BingNoMatch'
+        return False
+
+    def Open(self):
+        print ' '.join(self.url_args) + 'BingOpen'
+        sys.exit()
+
+    def Search(self):
+        if self.open_flag:
+            self.Open()
+        elif self.search_flag:
+            self.BingSearchResults(self.html)
+        elif self.f_lucky_flag:
+            self.FeelingLucky(self.html)        
+        elif self.wolfram_flag:
+            self.Wolfram()
         else:
-            print ('\n'.join(calc_result) + 'BingCalc').encode('ascii', 'ignore')
-        sys.exit(0)
-    elif define_result:
-        if len(define_result) == 1:
-            print (define_result[0] + 'BingCalc').encode('ascii', 'ignore')
-        else:
-            print ('\n'.join(define_result) + 'BingCalc').encode('ascii', 'ignore')
-        sys.exit(0)
-    else:
-        print 'BingNoMatch'
-        sys.exit(0)
-except AttributeError:
-    print 'BingNoMatch'
+            search_success = self.BingCalculation(self.html)
+            if not search_success:
+                self.Wolfram()
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", help="get bing search results",
+    action="store_true")
+    parser.add_argument("-f", "--flucky", help="feeling lucky result",
+    action="store_true")
+    parser.add_argument("-o", "--openurl", help="open link directly",
+    action="store_true")
+    parser.add_argument("-w", "--wolfram", help="get wolfram search result",
+    action="store_true")
+    parser.add_argument("URL_ARGS", nargs='*', help="Search URL arguments")
+    args = parser.parse_args()
+    search = bool(args.search)
+    flucky = bool(args.flucky)
+    openurl = bool(args.openurl)
+    wolfram = bool(args.wolfram)
+    bing_search = BingSearch(args.URL_ARGS, search, flucky, openurl, wolfram)
+    bing_search.Search()
+
 
--- wolfram.py
+++ wolfram.py
@@ -1,9 +1,10 @@
 ''' Python 2.7.3
 AUTHOR: Hunter Hammond
-VERSION: 1.8
+VERSION: 1.9
 DEPENDENCIES: lxml
 '''
 
+import argparse
 import os
 import sys
 import urllib2
@@ -12,63 +13,90 @@ from collections import OrderedDict
 
 import lxml.html as lh
 
-# Get API key from config.txt
-cfg_path = os.path.dirname(os.path.abspath(__file__))
-f = open(cfg_path + '/config.txt', 'r')
-api_key = f.readline()
-f.close()
 
-# Clean arguments
-arg_list = list(sys.argv)
-arg_list.pop(0) # Pop script name from list
-clean_args = []
-if arg_list:
-    for arg in arg_list:
+class WolfSearch:
+    def __init__(self, url_args):
+        self.url_args = self.ProcessArgs(url_args)
+        self.api_key = self.GetApiKey()
+        self.html = self.GetWolfHTML(self.url_args)
+    
+    def GetApiKey(self):
+        # Get API key from config.txt
+        cfg_path = os.path.dirname(os.path.abspath(__file__))
+        f = open(cfg_path + '/config.txt', 'r')
+        api_key = f.readline()
+        f.close()
+        return api_key
+
+    def ProcessArgs(self, url_args):
+        clean_args = []
+        if url_args:
+            for arg in url_args:
+                try:
+                    if " " in arg:
+                        clean_args = arg.split(" ")
+                    else:
+                        clean_args.append(arg)
+                except IndexError:
+                    sys.exit()
+        return clean_args
+
+    def GetWolfHTML(self, url_args):
+        # Add clean_args and api_key to base_url
+        base_url = 'http://api.wolframalpha.com/v2/query?input='
         try:
-            if arg[0] != "-":
-                if " " in arg:
-                    clean_args = arg.split(" ")
-                else:
-                    clean_args.append(arg)
-        except IndexError:
-            sys.exit(1)
+            clean_args = '+'.join(url_args)
+        except AttributeError:
+            sys.stderr.write("Argument list error! Expected list, got " + type(clean_args) + "\n")
+            sys.exit()
+        url = base_url + clean_args + '&appid=' + self.api_key
 
-# Add clean_args and api_key to base_url
-base_url = 'http://api.wolframalpha.com/v2/query?input='
-try:
-    url_args = '+'.join(clean_args)
-except AttributeError:
-    sys.stderr.write("Argument list error! Expected list, got " + type(clean_args) + "\n")
-    sys.exit(1)
-url = base_url + url_args + '&appid=' + api_key
-
-# Retrieve webpage response
-try:
-    request = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-    html = lh.parse(urllib2.urlopen(request))
-except urllib2.URLError:
-    print 'WolfFail'
-    sys.stderr.write('Failed to retrieve webpage.\n')
-    sys.exit(1)
-
-# Parse webpage response
-titles = list(OrderedDict.fromkeys(html.xpath("//pod[@title != '' and @title != 'Number line' and @title != 'Input' and @title != 'Visual representation']/@title")))
-entries = []
-if titles:
-    for title in titles:
-        entry_xpath = "//pod[@title='" + title + "']/subpod/plaintext/text()"
-        entry = html.xpath(entry_xpath)
-        if entry:
-            entries.append(entry[0])
-
-    entries = list(OrderedDict.fromkeys(entries))
-    output_list = []
-    for title, entry in itertools.izip(titles, entries):
+        # Retrieve webpage response
         try:
-            output_list.append(title + ': ' + entry).encode('ascii', 'ignore')
-        except (AttributeError, UnicodeEncodeError):
-            pass
-    print '\n'.join(output_list).encode('ascii', 'ignore')
-else:
-    print 'WolfNoMatch'
+            request = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+            return lh.parse(urllib2.urlopen(request))
+        except urllib2.URLError:
+            print 'WolfFail'
+            sys.stderr.write('Failed to retrieve webpage.\n')
+            sys.exit()
+    
+    def Search(self):
+        # Parse webpage response
+        titles = list(OrderedDict.fromkeys(self.html.xpath("//pod[@title != '' and @title != 'Number line' and @title != 'Input' and @title != 'Visual representation']/@title")))
+        entries = []
+        if titles:
+            for title in titles:
+                entry_xpath = "//pod[@title='" + title + "']/subpod/plaintext/text()"
+                entry = self.html.xpath(entry_xpath)
+                if entry:
+                    entries.append(entry[0])
+
+            entries = list(OrderedDict.fromkeys(entries))
+            output_list = []
+            for title, entry in itertools.izip(titles, entries):
+                try:
+                    output_list.append(title + ': ' + entry).encode('ascii', 'ignore')
+                except (AttributeError, UnicodeEncodeError):
+                    pass
+            print '\n'.join(output_list).encode('ascii', 'ignore')
+        else:
+            print 'WolfNoMatch'
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", help="get bing search results",
+    action="store_true")
+    parser.add_argument("-f", "--flucky", help="feeling lucky result",
+    action="store_true")
+    parser.add_argument("-o", "--openurl", help="open link directly",
+    action="store_true")
+    parser.add_argument("-w", "--wolfram", help="get wolfram search result",
+    action="store_true")
+    parser.add_argument("URL_ARGS", nargs='*', help="Search URL arguments")
+    args = parser.parse_args()
+    wolf_search = WolfSearch(args.URL_ARGS)
+    wolf_search.Search()
+
 
